86b1183c0f922d8541ebd4389f691815e068a4b6
freelanceradnan <reactorbro722@gmail.com>
2025-08-08 09:22:48 +0600

    printing odd number using filter methods in js

--- filter.js
+++ filter.js
@@ -1,9 +1,18 @@
 //using filter in js
 
-let nums=[1,2,3,4,5,6]
-let evenNums=nums.filter(function(element){
-    if(element%2==0){
-        return element;
+// let nums=[1,2,3,4,5,6]
+// let evenNums=nums.filter(function(element){
+//     if(element%2==0){
+//         return element;
+//     }
+// })
+// console.log(evenNums)
+
+//print odd nums using filter methods
+let nums=[1,2,3,4,5,6,7,8]
+let oddNums=nums.filter(function(element){
+    if(element%2!=0){
+        return element
     }
 })
-console.log(evenNums)
+console.log(oddNums)
