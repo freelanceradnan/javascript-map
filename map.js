6e322a1a4a65631843154b33ad6f6c11cdc82ac5
freelanceradnan <reactorbro722@gmail.com>
2025-08-08 06:53:55 +0600

    find all names with object with maps

--- map.js
+++ map.js
@@ -1,8 +1,45 @@
 //map in javascript
 //double number print
 
-let nums=[1,2,3,4,5];
-let doubleNums=nums.map(function(el){
-    return el*2;
+// let nums=[1,2,3,4,5];
+// let doubleNums=nums.map(function(el){
+//     return el*2;
+// })
+// console.log(doubleNums)
+
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//Get an array of all names
+let allNams=characters.map(function(el){
+    return el.name;
 })
-console.log(doubleNums)
+console.log(allNams)
