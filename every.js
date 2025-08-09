8028a51e028ec639eb3fe053c4294f01aa69c357
freelanceradnan <reactorbro722@gmail.com>
2025-08-09 07:29:19 +0600

    find true or flase from given question with every methods

--- every.js
+++ every.js
@@ -8,10 +8,49 @@
 // console.log(math);
 
 //print number with every even number(false)
-let nums=[1,2,3,4,5,6]
-let even=nums.every(function(el){
-    if(el%2==0){
+// let nums=[1,2,3,4,5,6]
+// let even=nums.every(function(el){
+//     if(el%2==0){
+//         return el
+//     }
+// })
+// console.log(even)
+
+const characters = [
+    {
+        name: 'Luke Skywalker',
+        height: '172',
+        mass: '77',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+    {
+        name: 'Darth Vader',
+        height: '202',
+        mass: '136',
+        eye_color: 'yellow',
+        gender: 'male',
+    },
+    {
+        name: 'Leia Organa',
+        height: '150',
+        mass: '49',
+        eye_color: 'brown',
+        gender: 'female',
+    },
+    {
+        name: 'Anakin Skywalker',
+        height: '188',
+        mass: '84',
+        eye_color: 'blue',
+        gender: 'male',
+    },
+];
+
+//Does every character have blue eyes?
+let blueEyes=characters.every(function(el){
+    if(el.eye_color=='blue'){
         return el
     }
 })
-console.log(even)
+console.log(blueEyes)
