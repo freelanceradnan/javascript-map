8b9b116224d9fec9e1ea2295b33c178dbf7151b0
freelanceradnan <reactorbro722@gmail.com>
2025-08-09 07:16:42 +0600

    print even number with every methods flase case

--- every.js
+++ every.js
@@ -1,8 +1,17 @@
 //every return boolean value
 //print a number with every double result
-let num=[1,2,3,4,5]
+// let num=[1,2,3,4,5]
 
-let math=num.every(function(el){
-    return el*2;
+// let math=num.every(function(el){
+//     return el*2;
+// })
+// console.log(math);
+
+//print number with every even number(false)
+let nums=[1,2,3,4,5,6]
+let even=nums.every(function(el){
+    if(el%2==0){
+        return el
+    }
 })
-console.log(math);
+console.log(even)
