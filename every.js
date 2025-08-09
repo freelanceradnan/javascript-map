998f0dd8cec68d2338074ff3f13f0e210f5cec4d
freelanceradnan <reactorbro722@gmail.com>
2025-08-09 07:10:32 +0600

    javascript every methods added

---
+++ every.js
@@ -0,0 +1,8 @@
+//every return boolean value
+//print a number with every double result
+let num=[1,2,3,4,5]
+
+let math=num.every(function(el){
+    return el*2;
+})
+console.log(math);
--- filter.js
+++ filter.js
@@ -1,21 +1,21 @@
 //using filter in js
 
 // let nums=[1,2,3,4,5,6]
-// let evenNums=nums.filter(function(element){
-//     if(element%2==0){
-//         return element;
-//     }
-// })
-// console.log(evenNums)
+let evenNums=nums.filter(function(element){
+    if(element%2==0){
+        return element;
+    }
+})
+console.log(evenNums)
 
 //print odd nums using filter methods
-// let nums=[1,2,3,4,5,6,7,8]
-// let oddNums=nums.filter(function(element){
-//     if(element%2!=0){
-//         return element
-//     }
-// })
-// console.log(oddNums)
+let nums=[1,2,3,4,5,6,7,8]
+let oddNums=nums.filter(function(element){
+    if(element%2!=0){
+        return element
+    }
+})
+console.log(oddNums)
 
 const characters = [
     {
@@ -49,13 +49,29 @@ const characters = [
 ];
 
 // Get characters with mass greater than 100
-// let massThen100=characters.filter(function(element){
-//     return element.mass>100
-// })
-// console.log(massThen100)
+let massThen100=characters.filter(function(element){
+    return element.mass>100
+})
+console.log(massThen100)
 
 //Get characters with height less than 200
 let heightLessThan200=characters.filter(function(element){
     return element.height<200;
 })
 console.log(heightLessThan200)
+
+// Get all male characters
+let allMaleCharecters=characters.filter(function(element){
+    if(element.gender=='male'){
+        return element
+    }
+})
+console.log(allMaleCharecters)
+
+//Get all female characters
+let allFemaleChar=characters.filter(function(element){
+    if(element.gender=='female'){
+        return element;
+    }
+})
+console.log(allFemaleChar)
