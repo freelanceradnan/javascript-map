2414fd7595c5001a7fd4a8498669f4c9423a6973
freelanceradnan <reactorbro722@gmail.com>
2025-08-09 07:56:16 +0600

    every methods in js

--- every.js
+++ every.js
@@ -1,21 +1,21 @@
 //every return boolean value
 //print a number with every double result
-// let num=[1,2,3,4,5]
+let num=[1,2,3,4,5]
 
-// let math=num.every(function(el){
-//     return el*2;
-// })
-// console.log(math);
+let math=num.every(function(el){
+    return el*2;
+})
+console.log(math);
 
 //print number with every even number(false)
-// let nums=[1,2,3,4,5,6]
-// let even=nums.every(function(el){
-//     if(el%2==0){
-//         return el
-//     }
-// })
-// console.log(even)
-
+let nums=[1,2,3,4,5,6]
+let even=nums.every(function(el){
+    if(el%2==0){
+        return el
+    }
+})
+console.log(even)
+//object and solve some question
 const characters = [
     {
         name: 'Luke Skywalker',
@@ -48,28 +48,28 @@ const characters = [
 ];
 
 //Does every character have blue eyes?
-// let blueEyes=characters.every(function(el){
-//     if(el.eye_color=='blue'){
-//         return el
-//     }
-// })
-// console.log(blueEyes)
+let blueEyes=characters.every(function(el){
+    if(el.eye_color=='blue'){
+        return el
+    }
+})
+console.log(blueEyes)
 
 // //Does every character have mass more than 40?
-// let mass40=characters.every(function(el){
-//     if(el.mass>=40){
-//         return el
-//     }
-// })
-// console.log(mass40)
+let mass40=characters.every(function(el){
+    if(el.mass>=40){
+        return el
+    }
+})
+console.log(mass40)
 
 // Is every character male?
-// let isMale=characters.every(function(el){
-//     if(el.gender=='male'){
-//         return el
-//     }
-// })
-// console.log(isMale)
+let isMale=characters.every(function(el){
+    if(el.gender=='male'){
+        return el
+    }
+})
+console.log(isMale)
 
 // Is every character shorter than 200?
 let shorterChar=characters.every(function(el){
