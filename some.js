32a3e646bb8f1270e914e08104655a8a2a3c62a9
freelanceradnan <reactorbro722@gmail.com>
2025-08-09 08:09:55 +0600

    finding some exist or not given array with every methods

--- some.js
+++ some.js
@@ -1,10 +1,20 @@
 //some provide only boolean value like every
 
-//find even num
-let nums=[1,2,3,4,5]
-let evenNum=nums.some(function(el){
-    if(el%2==0){
+//does array have even Num?
+// let nums=[1,2,3,4,5]
+// let evenNum=nums.some(function(el){
+//     if(el%2==0){
+//         return el
+//     }
+// })
+// console.log(evenNum)
+
+//
+//does array have odd Num?
+let nums=[1,2,3,4,5,6]
+let oddNums=nums.some(function(el){
+    if(el%2!=0){
         return el
     }
 })
-console.log(evenNum)
+console.log(oddNums)
