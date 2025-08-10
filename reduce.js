b0f5145fed8b8cb7a5809c970353da8b1df57c15
freelanceradnan <reactorbro722@gmail.com>
2025-08-10 13:21:24 +0600

    reduce methods in js

--- reduce.js
+++ reduce.js
@@ -1,10 +1,10 @@
 //reduce in js
 //sum of array element
-// let arr=[1,2,3,4,5]
-// let result=arr.reduce(function(acc,el){
-//     return acc+=el;
-// },0)
-// console.log(result)
+let arr=[1,2,3,4,5]
+let result=arr.reduce(function(acc,el){
+    return acc+=el;
+},0)
+console.log(result)
 
 //solving problems with reduce
 
@@ -45,30 +45,30 @@ const characters = [
 // Get the total number of characters by eye color (hint. a map of eye color to count)
 
 // // Get the total mass of all characters
-// let totalMass=characters.reduce(function(acc,el){
-//     return acc+=Number(el.mass)
-// },0)
-// console.log(totalMass)
+let totalMass=characters.reduce(function(acc,el){
+    return acc+=Number(el.mass)
+},0)
+console.log(totalMass)
 
 // Get the total height of all characters
-// let totalHeight=characters.reduce(function(acc,el){
-//     return acc+=Number(el.height);
-// },0)
-// console.log(totalHeight)
+let totalHeight=characters.reduce(function(acc,el){
+    return acc+=Number(el.height);
+},0)
+console.log(totalHeight)
 
 // Get the total number of characters in all the character names
 
-// let totalChar=characters.reduce(function(acc,el){
-//     return acc+=el.name.length;
-// },0)
-// console.log(totalChar)
+let totalChar=characters.reduce(function(acc,el){
+    return acc+=el.name.length;
+},0)
+console.log(totalChar)
 
 
 // // Get the total number of characters in all the character names(without space)
-// let totalChar=characters.reduce(function(acc,el){
-//     return acc+=el.name.split(' ').join('').length;
-// },0)
-// console.log(totalChar)
+let totalChar=characters.reduce(function(acc,el){
+    return acc+=el.name.split(' ').join('').length;
+},0)
+console.log(totalChar)
 
 // Get the total number of characters by eye color (hint. a map of eye color to count)
 
