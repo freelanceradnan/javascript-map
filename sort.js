fdc8482493670b64e636a476530207b2bd6ad8a5
freelanceradnan <reactorbro722@gmail.com>
2025-08-10 12:34:12 +0600

    shorting array number larger number to shorter number with sorting method

--- sort.js
+++ sort.js
@@ -1,8 +1,15 @@
 //short methods
 //shorting array number(shorter to langer number)
 
-let num=[30,23,12,344,2,4]
-let result=num.sort(function(a,b){
-    return a-b;
+// let num=[30,23,12,344,2,4]
+// let result=num.sort(function(a,b){
+//     return a-b;
+// })
+// console.log(result)
+
+//shorting array number(larger to shorter number)
+let arr=[200,2,23,21,234,211]
+let result=arr.sort(function(a,b){
+    return b-a;
 })
 console.log(result)
